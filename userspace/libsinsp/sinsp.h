860fd07d3bc1b577a43614e60d3566595660ba26
Joseph Pittman <79483176+jcpittman144@users.noreply.github.com>
2022-05-04 09:16:54 -0400

    Make sinsp remove_inactive_threads() method public (#54)
    * Make sinsp remove_inactive_threads() method public
    
    In the Sysdig agent's Secure_Light mode, the agent_diagnostic_metrics_sender
    functions as a lightweight version of the sinsp_analyzer, and needs similar
    access to the private method remove_inactive_threads().  This PR makes
    the method public.

--- userspace/libsinsp/sinsp.h
+++ userspace/libsinsp/sinsp.h
@@ -1010,6 +1010,8 @@ public:
 	bool get_track_connection_status();
 	void set_track_connection_status(bool enabled);
 
+	bool remove_inactive_threads();
+
 VISIBILITY_PROTECTED
 	bool add_thread(const sinsp_threadinfo *ptinfo);
 	void set_mode(scap_mode_t value)
@@ -1049,7 +1051,6 @@ private:
 
 	// this is here for testing purposes only
 	sinsp_threadinfo* find_thread_test(int64_t tid, bool lookup_only);
-	bool remove_inactive_threads();
 
 #if !defined(CYGWING_AGENT) && !defined(MINIMAL_BUILD)
 	void k8s_discover_ext();
