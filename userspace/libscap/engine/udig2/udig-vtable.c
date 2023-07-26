dcd4c3ad413d14f482f2e85aa33cfe1f43d6b864
Wiktor Golgowski <wiktor.golgowski@sysdig.com>
2023-07-26 15:43:48 +0000

    cleanup(libscap): align udig2 with platform changes
    Signed-off-by: Wiktor Golgowski <wiktor.golgowski@sysdig.com>

--- userspace/libscap/engine/udig2/udig-vtable.c
+++ userspace/libscap/engine/udig2/udig-vtable.c
@@ -24,10 +24,6 @@ const struct scap_vtable scap_udig_engine = {
 	.get_n_tracepoint_hit = scap_udig_get_n_tracepoint_hit,
 	.get_n_devs = scap_udig_get_n_devs,
 	.get_max_buf_used = scap_udig_get_max_buf_used,
-	.get_threadlist = scap_procfs_get_threadlist,
-	.get_vpid = noop_get_vxid,
-	.get_vtid = noop_get_vxid,
-	.getpid_global = noop_getpid_global,
 	.get_api_version = NULL,
 	.get_schema_version = NULL,
 };
