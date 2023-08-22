kirby-mimic/agent-libs

Resolve build errors from 0.10.4 merge

userspace/libsinsp/filterchecks.cpp
@@ -3950,7 +3950,7 @@ uint8_t* sinsp_filter_check_event::extract(sinsp_evt *evt, OUT uint32_t* len, bo
 				uint16_t evid = *(uint16_t *)parinfo->m_val;
 
 				// Only generic enter event has the nativeID as second param
-				if (m_inspector->is_capture() && evid == PPM_SC_UNKNOWN && etype == PPME_GENERIC_E)
+				if (m_inspector && m_inspector->is_capture() && evid == PPM_SC_UNKNOWN && etype == PPME_GENERIC_E)
 				{
 					// try to enforce a forward compatibility for syscalls added
 					// after a scap file was generated,

mattnite/falcosecurity-libs

userspace/libsinsp/filterchecks.cpp
@@ -5122,7 +5122,8 @@ uint8_t* sinsp_filter_check_event::extract(sinsp_evt *evt, OUT uint32_t* len, bo
 				uint16_t ppm_sc = *(uint16_t *)parinfo->m_val;
 
 				// Only generic enter event has the nativeID as second param
-				if(m_inspector->is_capture() && ppm_sc == PPM_SC_UNKNOWN && etype == PPME_GENERIC_E)
+
+				if(m_inspector && m_inspector->is_capture() && ppm_sc == PPM_SC_UNKNOWN && etype == PPME_GENERIC_E)
 				{
 					// try to enforce a forward compatibility for syscalls added
 					// after a scap file was generated,