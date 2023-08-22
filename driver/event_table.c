therealbobo/libs

feat(driver): support for init_module,finit_module syscalls
Signed-off-by: Roberto Scolaro <roberto.scolaro21@gmail.com>

driver/event_table.c
@@ -459,6 +459,10 @@ const struct ppm_event_info g_event_info[] = {
 	[PPME_SYSCALL_PIDFD_GETFD_X] = {"pidfd_getfd", EC_PROCESS | EC_SYSCALL, EF_CREATES_FD , 4, {{"fd", PT_FD, PF_DEC}, {"pid_fd", PT_FD, PF_DEC}, {"target_fd", PT_FD, PF_DEC}, {"flags", PT_FLAGS32, PF_HEX}}},
 	[PPME_SYSCALL_PIDFD_OPEN_E] = {"pidfd_open", EC_PROCESS | EC_SYSCALL, EF_CREATES_FD | EF_MODIFIES_STATE, 0},
 	[PPME_SYSCALL_PIDFD_OPEN_X] = {"pidfd_open", EC_PROCESS | EC_SYSCALL, EF_CREATES_FD | EF_MODIFIES_STATE, 3, {{"fd", PT_FD, PF_DEC}, {"pid", PT_PID, PF_DEC}, {"flags", PT_FLAGS32, PF_HEX}}}, 
+	[PPME_SYSCALL_INIT_MODULE_E] = {"init_module", EC_OTHER | EC_SYSCALL, EF_NONE, 0},
+	[PPME_SYSCALL_INIT_MODULE_X] = {"init_module", EC_OTHER | EC_SYSCALL, EF_NONE, 4, {{"res", PT_ERRNO, PF_DEC}, {"img", PT_BYTEBUF, PF_NA}, {"length", PT_UINT64, PF_DEC}, {"uargs", PT_CHARBUF, PF_NA}}},
+	[PPME_SYSCALL_FINIT_MODULE_E] = {"finit_module", EC_OTHER | EC_SYSCALL, EF_NONE, 0},
+	[PPME_SYSCALL_FINIT_MODULE_X] = {"finit_module", EC_OTHER | EC_SYSCALL, EF_USES_FD | EF_READS_FROM_FD, 4, {{"res", PT_ERRNO, PF_DEC}, {"fd", PT_FD, PF_DEC}, {"uargs", PT_CHARBUF, PF_NA}, {"flags", PT_INT32, PF_DEC}}},
 };
 
 // We don't need this check in kmod (this source file is included during kmod compilation!)

driver/modern_bpf/programs/tail_called/events/syscall_dispatched_events/init_module.bpf.c
@@ -1,0 +1,85 @@
+/*
+ * Copyright (C) 2023 The Falco Authors.
+ *
+ * This file is dual licensed under either the MIT or GPL 2. See MIT.txt
+ * or GPL2.txt for full copies of the license.
+ */
+
+#include <helpers/interfaces/fixed_size_event.h>
+#include <helpers/interfaces/variable_size_event.h>
+
+/*=============================== ENTER EVENT ===========================*/
+
+SEC("tp_btf/sys_enter")
+int BPF_PROG(init_module_e,
+	     struct pt_regs *regs,
+	     long id)
+{
+	struct ringbuf_struct ringbuf;
+	if(!ringbuf__reserve_space(&ringbuf, ctx, INIT_MODULE_E_SIZE, PPME_SYSCALL_INIT_MODULE_E))
+	{
+		return 0;
+	}
+
+	ringbuf__store_event_header(&ringbuf);
+
+	/*=============================== COLLECT PARAMETERS  ===========================*/
+
+	// Here we have no parameters to collect.
+
+	/*=============================== COLLECT PARAMETERS  ===========================*/
+
+	ringbuf__submit_event(&ringbuf);
+
+	return 0;
+
+
+}
+
+/*=============================== ENTER EVENT ===========================*/
+
+/*=============================== EXIT EVENT ===========================*/
+
+SEC("tp_btf/sys_exit")
+int BPF_PROG(init_module_x,
+	     struct pt_regs *regs,
+	     long ret)
+{
+	struct auxiliary_map *auxmap = auxmap__get();
+	if(!auxmap)
+	{
+		return 0;
+	}
+
+	auxmap__preload_event_header(auxmap, PPME_SYSCALL_INIT_MODULE_X);
+
+	/*=============================== COLLECT PARAMETERS  ===========================*/
+
+	/* Parameter 1: ret (type: PT_ERRNO) */
+	auxmap__store_s64_param(auxmap, ret);
+
+	/* Parameter 3: length (type: PT_UINT64) */
+	u64 len = extract__syscall_argument(regs, 1);
+
+	/* Parameter 2: img (type: PT_BYTEBUF) */
+	unsigned long img_ptr = extract__syscall_argument(regs, 0);
+	auxmap__store_bytebuf_param(auxmap, img_ptr, len, USER);
+
+	/* Parameter 3: length (type: PT_UINT64) */
+	auxmap__store_u64_param(auxmap, (u64)len);
+
+	/* Parameter 4: uargs (type: PT_CHARBUF) */
+	unsigned long uargs_ptr = extract__syscall_argument(regs, 2);
+	auxmap__store_charbuf_param(auxmap, uargs_ptr, len, USER);
+
+
+	/*=============================== COLLECT PARAMETERS  ===========================*/
+
+	auxmap__finalize_event_header(auxmap);
+
+	auxmap__submit_event(auxmap, ctx);
+
+	return 0;
+}
+
+/*=============================== EXIT EVENT ===========================*/

test/drivers/test_suites/syscall_enter_suite/finit_module_e.cpp
@@ -1,0 +1,41 @@
+#include "../../event_class/event_class.h"
+#if defined(__NR_finit_module)
+TEST(SyscallEnter, finit_moduleE)
+{
+	auto evt_test = get_syscall_event_test(__NR_finit_module, ENTER_EVENT);
+	evt_test->enable_capture();
+
+	/*=============================== TRIGGER SYSCALL  ===========================*/
+
+	int mock_fd = -1;
+	char mock_buf[8];
+	int mock_flags = 0;
+
+	assert_syscall_state(SYSCALL_SUCCESS, "finit_module", syscall(__NR_finit_module, mock_fd, (void *)(mock_buf), mock_flags));
+
+
+	/*=============================== TRIGGER SYSCALL  ===========================*/
+
+	evt_test->disable_capture();
+
+	evt_test->assert_event_presence();
+
+	if(HasFatalFailure())
+	{
+		return;
+	}
+
+	evt_test->parse_event();
+
+	evt_test->assert_header();
+
+	/*=============================== ASSERT PARAMETERS  ===========================*/
+
+	// Here we have no parameters to assert.
+
+	/*=============================== ASSERT PARAMETERS  ===========================*/
+
+	evt_test->assert_num_params_pushed(0);
+
+}
+#endif

test/drivers/test_suites/syscall_enter_suite/init_module_e.cpp
@@ -1,0 +1,41 @@
+#include "../../event_class/event_class.h"
+#if defined(__NR_init_module)
+TEST(SyscallEnter, init_moduleE)
+{
+	auto evt_test = get_syscall_event_test(__NR_init_module, ENTER_EVENT);
+	evt_test->enable_capture();
+
+	/*=============================== TRIGGER SYSCALL  ===========================*/
+
+	char mock_img[100];
+	unsigned long len = 100;
+	char mock_buf[8];
+
+	assert_syscall_state(SYSCALL_SUCCESS, "init_module", syscall(__NR_init_module, (void *)(mock_img), len, (void *)(mock_buf)));
+
+
+	/*=============================== TRIGGER SYSCALL  ===========================*/
+
+	evt_test->disable_capture();
+
+	evt_test->assert_event_presence();
+
+	if(HasFatalFailure())
+	{
+		return;
+	}
+
+	evt_test->parse_event();
+
+	evt_test->assert_header();
+
+	/*=============================== ASSERT PARAMETERS  ===========================*/
+
+	// Here we have no parameters to assert.
+
+	/*=============================== ASSERT PARAMETERS  ===========================*/
+
+	evt_test->assert_num_params_pushed(0);
+
+}
+#endif

test/drivers/test_suites/syscall_exit_suite/finit_module_x.cpp
@@ -1,0 +1,60 @@
+#include "../../event_class/event_class.h"
+
+#if defined(__NR_finit_module)
+
+#if defined(__NR_close) && defined(__NR_open) && defined(__NR_read)
+TEST(SyscallExit, finit_moduleX_failure)
+{
+	auto evt_test = get_syscall_event_test(__NR_finit_module, EXIT_EVENT);
+	evt_test->enable_capture();
+
+	/*=============================== TRIGGER SYSCALL  ===========================*/
+
+	/* Read data from /dev/urandom */
+	char mock_buf[] = "AAABAACAADAAEAAFAAGAAHAAIAAJAAKAALAAMAA\0";
+
+	/*
+	 * Call the `finit_module`
+	 */
+
+	int64_t kmod_fd = 99;
+	assert_syscall_state(SYSCALL_FAILURE, "finit_module", syscall(__NR_finit_module, kmod_fd, (void*)mock_buf, 1));
+	int64_t errno_value = -errno;
+
+
+	/*=============================== TRIGGER SYSCALL  ===========================*/
+
+	evt_test->disable_capture();
+
+	evt_test->assert_event_presence();
+
+	if(HasFatalFailure())
+	{
+		return;
+	}
+
+	evt_test->parse_event();
+
+	evt_test->assert_header();
+
+	/*=============================== ASSERT PARAMETERS  ===========================*/
+
+	/* Parameter 1: res (type: PT_ERRNO)*/
+	evt_test->assert_numeric_param(1, (uint64_t)errno_value);
+
+	/* Parameter 2: fd (type: PT_FD) */
+	evt_test->assert_numeric_param(2, (uint64_t)kmod_fd);
+
+	/* Parameter 3: uargs (type: PT_CHARBUF) */
+	evt_test->assert_charbuf_param(3, mock_buf);
+
+	/* Parameter 4: flags (type: PT_UINT64) */
+	evt_test->assert_numeric_param(4, 1);
+
+	/*=============================== ASSERT PARAMETERS  ===========================*/
+
+	evt_test->assert_num_params_pushed(4);
+}
+#endif
+
+#endif

mattnite/falcosecurity-libs

Co-Authored-By: Andrea Terzolo <andreaterzolo3@gmail.com>

driver/event_table.c
@@ -459,6 +459,10 @@ const struct ppm_event_info g_event_info[] = {
 	[PPME_SYSCALL_PIDFD_GETFD_X] = {"pidfd_getfd", EC_PROCESS | EC_SYSCALL, EF_CREATES_FD , 4, {{"fd", PT_FD, PF_DEC}, {"pid_fd", PT_FD, PF_DEC}, {"target_fd", PT_FD, PF_DEC}, {"flags", PT_FLAGS32, PF_HEX}}},
 	[PPME_SYSCALL_PIDFD_OPEN_E] = {"pidfd_open", EC_PROCESS | EC_SYSCALL, EF_CREATES_FD | EF_MODIFIES_STATE, 0},
 	[PPME_SYSCALL_PIDFD_OPEN_X] = {"pidfd_open", EC_PROCESS | EC_SYSCALL, EF_CREATES_FD | EF_MODIFIES_STATE, 3, {{"fd", PT_FD, PF_DEC}, {"pid", PT_PID, PF_DEC}, {"flags", PT_FLAGS32, PF_HEX}}}, 
+	[PPME_SYSCALL_INIT_MODULE_E] = {"init_module", EC_OTHER | EC_SYSCALL, EF_NONE, 0},
+	[PPME_SYSCALL_INIT_MODULE_X] = {"init_module", EC_OTHER | EC_SYSCALL, EF_NONE, 4, {{"res", PT_ERRNO, PF_DEC}, {"img", PT_BYTEBUF, PF_NA}, {"length", PT_UINT64, PF_DEC}, {"uargs", PT_CHARBUF, PF_NA}}},
+	[PPME_SYSCALL_FINIT_MODULE_E] = {"finit_module", EC_OTHER | EC_SYSCALL, EF_NONE, 0},
+	[PPME_SYSCALL_FINIT_MODULE_X] = {"finit_module", EC_OTHER | EC_SYSCALL, EF_USES_FD | EF_READS_FROM_FD, 4, {{"res", PT_ERRNO, PF_DEC}, {"fd", PT_FD, PF_DEC}, {"uargs", PT_CHARBUF, PF_NA}, {"flags", PT_FLAGS32, PF_DEC}}},
 };
 
 // We don't need this check in kmod (this source file is included during kmod compilation!)

driver/modern_bpf/programs/tail_called/events/syscall_dispatched_events/init_module.bpf.c
@@ -1,0 +1,84 @@
+/*
+ * Copyright (C) 2023 The Falco Authors.
+ *
+ * This file is dual licensed under either the MIT or GPL 2. See MIT.txt
+ * or GPL2.txt for full copies of the license.
+ */
+
+#include <helpers/interfaces/fixed_size_event.h>
+#include <helpers/interfaces/variable_size_event.h>
+
+/*=============================== ENTER EVENT ===========================*/
+
+SEC("tp_btf/sys_enter")
+int BPF_PROG(init_module_e,
+	     struct pt_regs *regs,
+	     long id)
+{
+	struct ringbuf_struct ringbuf;
+	if(!ringbuf__reserve_space(&ringbuf, ctx, INIT_MODULE_E_SIZE, PPME_SYSCALL_INIT_MODULE_E))
+	{
+		return 0;
+	}
+
+	ringbuf__store_event_header(&ringbuf);
+
+	/*=============================== COLLECT PARAMETERS  ===========================*/
+
+	// Here we have no parameters to collect.
+
+	/*=============================== COLLECT PARAMETERS  ===========================*/
+
+	ringbuf__submit_event(&ringbuf);
+
+	return 0;
+
+
+}
+
+/*=============================== ENTER EVENT ===========================*/
+
+/*=============================== EXIT EVENT ===========================*/
+
+SEC("tp_btf/sys_exit")
+int BPF_PROG(init_module_x,
+	     struct pt_regs *regs,
+	     long ret)
+{
+	struct auxiliary_map *auxmap = auxmap__get();
+	if(!auxmap)
+	{
+		return 0;
+	}
+
+	auxmap__preload_event_header(auxmap, PPME_SYSCALL_INIT_MODULE_X);
+
+	/*=============================== COLLECT PARAMETERS  ===========================*/
+
+	/* Parameter 1: ret (type: PT_ERRNO) */
+	auxmap__store_s64_param(auxmap, ret);
+
+	u64 len = extract__syscall_argument(regs, 1);
+
+	/* Parameter 2: img (type: PT_BYTEBUF) */
+	unsigned long img_ptr = extract__syscall_argument(regs, 0);
+	auxmap__store_bytebuf_param(auxmap, img_ptr, len, USER);
+
+	/* Parameter 3: length (type: PT_UINT64) */
+	auxmap__store_u64_param(auxmap, (u64)len);
+
+	/* Parameter 4: uargs (type: PT_CHARBUF) */
+	unsigned long uargs_ptr = extract__syscall_argument(regs, 2);
+	auxmap__store_charbuf_param(auxmap, uargs_ptr, MAX_PROC_ARG_ENV, USER);
+
+
+	/*=============================== COLLECT PARAMETERS  ===========================*/
+
+	auxmap__finalize_event_header(auxmap);
+
+	auxmap__submit_event(auxmap, ctx);
+
+	return 0;
+}
+
+/*=============================== EXIT EVENT ===========================*/

test/drivers/test_suites/syscall_enter_suite/finit_module_e.cpp
@@ -1,0 +1,41 @@
+#include "../../event_class/event_class.h"
+#if defined(__NR_finit_module)
+TEST(SyscallEnter, finit_moduleE)
+{
+	auto evt_test = get_syscall_event_test(__NR_finit_module, ENTER_EVENT);
+	evt_test->enable_capture();
+
+	/*=============================== TRIGGER SYSCALL  ===========================*/
+
+	int mock_fd = -1;
+	char mock_buf[8];
+	int mock_flags = 0;
+
+	assert_syscall_state(SYSCALL_FAILURE, "finit_module", syscall(__NR_finit_module, mock_fd, (void *)(mock_buf), mock_flags));
+
+
+	/*=============================== TRIGGER SYSCALL  ===========================*/
+
+	evt_test->disable_capture();
+
+	evt_test->assert_event_presence();
+
+	if(HasFatalFailure())
+	{
+		return;
+	}
+
+	evt_test->parse_event();
+
+	evt_test->assert_header();
+
+	/*=============================== ASSERT PARAMETERS  ===========================*/
+
+	// Here we have no parameters to assert.
+
+	/*=============================== ASSERT PARAMETERS  ===========================*/
+
+	evt_test->assert_num_params_pushed(0);
+
+}
+#endif

test/drivers/test_suites/syscall_enter_suite/init_module_e.cpp
@@ -1,0 +1,41 @@
+#include "../../event_class/event_class.h"
+#if defined(__NR_init_module)
+TEST(SyscallEnter, init_moduleE)
+{
+	auto evt_test = get_syscall_event_test(__NR_init_module, ENTER_EVENT);
+	evt_test->enable_capture();
+
+	/*=============================== TRIGGER SYSCALL  ===========================*/
+
+	char mock_img[100];
+	unsigned long len = 100;
+	char mock_buf[8];
+
+	assert_syscall_state(SYSCALL_FAILURE, "init_module", syscall(__NR_init_module, (void *)(mock_img), len, (void *)(mock_buf)));
+
+
+	/*=============================== TRIGGER SYSCALL  ===========================*/
+
+	evt_test->disable_capture();
+
+	evt_test->assert_event_presence();
+
+	if(HasFatalFailure())
+	{
+		return;
+	}
+
+	evt_test->parse_event();
+
+	evt_test->assert_header();
+
+	/*=============================== ASSERT PARAMETERS  ===========================*/
+
+	// Here we have no parameters to assert.
+
+	/*=============================== ASSERT PARAMETERS  ===========================*/
+
+	evt_test->assert_num_params_pushed(0);
+
+}
+#endif

test/drivers/test_suites/syscall_exit_suite/finit_module_x.cpp
@@ -1,0 +1,57 @@
+#include "../../event_class/event_class.h"
+
+#if defined(__NR_finit_module)
+
+TEST(SyscallExit, finit_moduleX_failure)
+{
+	auto evt_test = get_syscall_event_test(__NR_finit_module, EXIT_EVENT);
+	evt_test->enable_capture();
+
+	/*=============================== TRIGGER SYSCALL  ===========================*/
+
+	char mock_buf[] = "AAABAACAADAAEAAFAAGAAHAAIAAJAAKAALAAMAA\0";
+
+	/*
+	 * Call the `finit_module`
+	 */
+
+	int64_t kmod_fd = 99;
+	assert_syscall_state(SYSCALL_FAILURE, "finit_module", syscall(__NR_finit_module, kmod_fd, (void*)mock_buf, 1));
+	int64_t errno_value = -errno;
+
+
+	/*=============================== TRIGGER SYSCALL  ===========================*/
+
+	evt_test->disable_capture();
+
+	evt_test->assert_event_presence();
+
+	if(HasFatalFailure())
+	{
+		return;
+	}
+
+	evt_test->parse_event();
+
+	evt_test->assert_header();
+
+	/*=============================== ASSERT PARAMETERS  ===========================*/
+
+	/* Parameter 1: res (type: PT_ERRNO)*/
+	evt_test->assert_numeric_param(1, (uint64_t)errno_value);
+
+	/* Parameter 2: fd (type: PT_FD) */
+	evt_test->assert_numeric_param(2, (int64_t)kmod_fd);
+
+	/* Parameter 3: uargs (type: PT_CHARBUF) */
+	evt_test->assert_charbuf_param(3, mock_buf);
+
+	/* Parameter 4: flags (type: PT_UINT64) */
+	evt_test->assert_numeric_param(4, 1);
+
+	/*=============================== ASSERT PARAMETERS  ===========================*/
+
+	evt_test->assert_num_params_pushed(4);
+}
+
+#endif